be4cc7d02c90dc9ebb45bfe4070a5a8a4f168bff
muneerashaik <muneera.shaik@ibhubs.co.in>
2021-08-23 11:30:12 +0530

    Updates

---
+++ precache-manifest.db987e205563f812db3eee9a71b931d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e2d83ef0256457af2ebb98ae898fb3",
+    "url": "/test-react-component-lib/index.html"
+  },
+  {
+    "revision": "a73b026cfbe1b9ac0da5",
+    "url": "/test-react-component-lib/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "b026650995e386f343d3",
+    "url": "/test-react-component-lib/static/js/2.0f0a203a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/test-react-component-lib/static/js/2.0f0a203a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a73b026cfbe1b9ac0da5",
+    "url": "/test-react-component-lib/static/js/main.1063574e.chunk.js"
+  },
+  {
+    "revision": "9ad863ac54f4f8824643",
+    "url": "/test-react-component-lib/static/js/runtime-main.9624f50f.js"
+  }
+]);
